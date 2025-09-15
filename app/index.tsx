cc891749016c7e0443e4cb7f839413df7a1b0924
Michaela Poštulka <michaelamosova@gmail.com>
2025-09-15 21:16:20 +0200

    feat: refetch gif every 10 seconds, error + loading state on homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '@/components/Header';
 import SearchBar from '@/components/SearchBar';
 import GifDetail from '@/components/GifDetail';
 import { fetchRandomGif } from '@/services/api';
 import useFetch from '@/hooks/useFetch';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react';
 
 export default function Index() {
   // TODO @mpostulka - handle loading and error states
@@ -12,10 +14,19 @@ export default function Index() {
     data: gif,
     loading: gifLoading,
     error: gifError,
+    refetch: gifRefetch,
   } = useFetch(() => fetchRandomGif());
 
+  // refetch only when screen is focused, with useCallback for dependency array
+  useFocusEffect(
+    useCallback(() => {
+      const id = setInterval(gifRefetch, 10_000);
+      return () => clearInterval(id);
+    }, [gifRefetch]),
+  );
+
   return (
-    <SafeAreaView className="flex-1 bg-background-dark justify-start items-center px-4">
+    <SafeAreaView className="flex-1 justify-start items-center bg-background-dark px-4">
       <Header
         title={'GIF Explorer'}
         subtitle={'Discover and search amazing GIFs'}
@@ -24,12 +35,25 @@ export default function Index() {
         <SearchBar />
       </View>
 
-      <View className="flex-1 justify-center items-start w-full mt-10">
-        <Text className="color-primary text-base font-light">
-          Random selected GIF:
-        </Text>
-        {gif && <GifDetail gif={gif} />}
-      </View>
+      <>
+        {gif && !gifLoading && !gifError ? (
+          <View className="flex-1 justify-center items-start w-full mt-10">
+            <Text className="color-primary text-base font-light">
+              Random selected GIF:
+            </Text>
+            <GifDetail gif={gif} />
+          </View>
+        ) : gifLoading && !gifError ? (
+          <View className="items-center justify-center mt-16">
+            <ActivityIndicator size="large" />
+            <Text className="text-primary mt-4 opacity-70">Loading…</Text>
+          </View>
+        ) : (
+          <Text className="text-primary text-2xl font-bold mt-8">
+            {gifError?.message ?? 'Error when loading random GIF.'}
+          </Text>
+        )}
+      </>
     </SafeAreaView>
   );
 }
